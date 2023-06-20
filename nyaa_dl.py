7ccfb1209e06c05d9175446f959bed47fab79af0
martianbit <martianbit@proton.me>
2023-06-20 11:22:00 +0200

    switch to space indentation, get python from env and use dash in the installed executable

--- nyaa_dl.py
+++ nyaa_dl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from math import floor, log
 from time import sleep
@@ -11,8 +11,8 @@ import requests
 from qbittorrent import Client
 
 class Mode(Enum):
-	SEARCH = auto()
-	AUTO = auto()
+    SEARCH = auto()
+    AUTO = auto()
 
 KW_1 = "<a href=\"/download/"
 KW_2 = "title=\""
@@ -24,217 +24,217 @@ vpath = "./S1"
 parser = ArgumentParser()
 
 parser.add_argument("-s", "--start", type=int,
-	                help="start ep index, defaults to next ep in vpath")
+                    help="start ep index, defaults to next ep in vpath")
 
 parser.add_argument("-e", "--end", type=int,
-	                help="end ep index, defaults to start")
+                    help="end ep index, defaults to start")
 
 parser.add_argument("-d", "--digits", type=int,
-	                help="digits in ep index, defaults to 2")
+                    help="digits in ep index, defaults to 2")
 
 parser.add_argument("-p", "--tpath", type=str,
-	                help=".torrent files go here, defaults to \"./torrents\"")
+                    help=".torrent files go here, defaults to \"./torrents\"")
 
 parser.add_argument("-P", "--vpath", type=str,
-	                help="qb should save the vid files here, defaults to ./S1")
+                    help="qb should save the vid files here, defaults to ./S1")
 
 parser.add_argument("search_term", type=str,
-	                help="if contains a '*' symbol, auto mode (ep i put there), else, search mode")
+                    help="if contains a '*' symbol, auto mode (ep i put there), else, search mode")
 
 args = parser.parse_args()
 
 search_term = args.search_term
 
 if args.tpath != None:
-	tpath = args.tpath
+    tpath = args.tpath
 
 if args.vpath != None:
-	vpath = args.vpath
+    vpath = args.vpath
 
 vpath = abspath(vpath)
 
 mode = None
 
 if "*" in search_term:
-	mode = Mode.AUTO
+    mode = Mode.AUTO
 else:
-	mode = Mode.SEARCH
+    mode = Mode.SEARCH
 
 try:
-	qb = Client("http://localhost:1317")
+    qb = Client("http://localhost:1317")
 except:
-	print("pls start qb")
-	exit(1)
+    print("pls start qb")
+    exit(1)
 
 def fill0(n, d):
-	r = str(n)
+    r = str(n)
 
-	for i in range(floor(log(n, 10)) + 1, d):
-		r = "0" + r
+    for i in range(floor(log(n, 10)) + 1, d):
+        r = "0" + r
 
-	return r
+    return r
 
 def check_dir(dp, name):
-	if not isdir(dp):
-		print("no such dir: {} ({})".format(dp, name))
-		exit(1)
+    if not isdir(dp):
+        print("no such dir: {} ({})".format(dp, name))
+        exit(1)
 
 def send_req(url):
-	res = None
+    res = None
 
-	success = False
+    success = False
 
-	while True:
-		try:
-			res = requests.get(url, timeout=10)
-			success = (res.status_code == 200)
-		except:
-			pass
+    while True:
+        try:
+            res = requests.get(url, timeout=10)
+            success = (res.status_code == 200)
+        except:
+            pass
 
-		if success:
-			break
-		else:
-			print("net err, retrying")
-			sleep(5)
+        if success:
+            break
+        else:
+            print("net err, retrying")
+            sleep(5)
 
-	return res
+    return res
 
 def fetch_feed(cst):
-	suburl = cst.replace(" ", "+")
+    suburl = cst.replace(" ", "+")
 
-	print("fetching feed")
-	sr = send_req("https://nyaa.si/?f=0&c=0_0&q={}&s=seeders&o=desc".format(suburl))
+    print("fetching feed")
+    sr = send_req("https://nyaa.si/?f=0&c=0_0&q={}&s=seeders&o=desc".format(suburl))
 
-	return sr.text
+    return sr.text
 
 def dl_by_id(_id):
-	tfp = join(tpath, _id + ".torrent")
+    tfp = join(tpath, _id + ".torrent")
 
-	print("checking if .torrent file is already down... ", end="")
+    print("checking if .torrent file is already down... ", end="")
 
-	if isfile(tfp):
-		print("yes")
-	else:
-		print("no")
+    if isfile(tfp):
+        print("yes")
+    else:
+        print("no")
 
-		print("dling .torrent file")
-		tr = send_req("https://nyaa.si/download/{}.torrent".format(_id))
+        print("dling .torrent file")
+        tr = send_req("https://nyaa.si/download/{}.torrent".format(_id))
 
-		print("saving .torrent file")
+        print("saving .torrent file")
 
-		tfw = open(tfp, "wb")
-		tfw.write(tr.content)
-		tfw.close()
+        tfw = open(tfp, "wb")
+        tfw.write(tr.content)
+        tfw.close()
 
-	print("sending .torrent file to qb")
+    print("sending .torrent file to qb")
 
-	tfr = open(tfp, "rb")
-	qb.download_from_file(tfr, savepath=vpath)
-	tfr.close()
+    tfr = open(tfp, "rb")
+    qb.download_from_file(tfr, savepath=vpath)
+    tfr.close()
 
 check_dir(tpath, "tpath")
 check_dir(vpath, "vpath")
 
 if mode == Mode.AUTO:
-	start = 1
-	end = None
-	digits = 2
+    start = 1
+    end = None
+    digits = 2
 
-	if args.digits != None:
-		digits = args.digits
+    if args.digits != None:
+        digits = args.digits
 
-	if args.start == None:
-		bnr = re.compile("^E[0-9]{{1,{}}}\..*$".format(digits))
+    if args.start == None:
+        bnr = re.compile("^E[0-9]{{1,{}}}\..*$".format(digits))
 
-		vfs = glob(join(vpath, "*"))
-		ns = False
+        vfs = glob(join(vpath, "*"))
+        ns = False
 
-		for x in vfs:
-			bn = basename(x)
+        for x in vfs:
+            bn = basename(x)
 
-			if not bnr.match(bn):
-				continue
+            if not bnr.match(bn):
+                continue
 
-			try:
-				start = max(int(bn[1:(bn.index("."))]), start)
-				ns = True
-			except:
-				pass
+            try:
+                start = max(int(bn[1:(bn.index("."))]), start)
+                ns = True
+            except:
+                pass
 
-		start += ns
-	else:
-		start = args.start
+        start += ns
+    else:
+        start = args.start
 
-	if args.end == None:
-		end = start
-	else:
-		end = args.end
+    if args.end == None:
+        end = start
+    else:
+        end = args.end
 
-	print("scraping eps from E{} to E{}".format(start, end))
+    print("scraping eps from E{} to E{}".format(start, end))
 
-	for i in range(start, end + 1):
-		print("next target is E" + str(i))
+    for i in range(start, end + 1):
+        print("next target is E" + str(i))
 
-		cst = search_term.replace("*", fill0(i, digits))
+        cst = search_term.replace("*", fill0(i, digits))
 
-		st = fetch_feed(cst)
+        st = fetch_feed(cst)
 
-		id_si = id_ei = None
+        id_si = id_ei = None
 
-		try:
-			ti = st.index(cst, st.index("<div class=\"table-responsive\">"))
+        try:
+            ti = st.index(cst, st.index("<div class=\"table-responsive\">"))
 
-			id_si = st.index(KW_1, ti) + len(KW_1)
-			id_ei = st.index(".torrent", id_si)
-		except:
-			print("no match, skipping")
-			continue
+            id_si = st.index(KW_1, ti) + len(KW_1)
+            id_ei = st.index(".torrent", id_si)
+        except:
+            print("no match, skipping")
+            continue
 
-		_id = st[id_si:id_ei]
+        _id = st[id_si:id_ei]
 
-		print("match, id is " + _id)
-		dl_by_id(_id)
+        print("match, id is " + _id)
+        dl_by_id(_id)
 elif mode == Mode.SEARCH:
-	st = fetch_feed(search_term)
+    st = fetch_feed(search_term)
 
-	rs = []
-	pi = 0
+    rs = []
+    pi = 0
 
-	while True:
-		tt_si = tt_ei = None
-		id_si = id_ei = None
+    while True:
+        tt_si = tt_ei = None
+        id_si = id_ei = None
 
-		try:
-			vi = st.index(KW_3, pi)
-			vj = st.index("\"", vi + len(KW_3))
+        try:
+            vi = st.index(KW_3, pi)
+            vj = st.index("\"", vi + len(KW_3))
 
-			if st[vj - 1] == "s":
-				vi = st.index("<a", vi);
+            if st[vj - 1] == "s":
+                vi = st.index("<a", vi);
 
-			tt_si = st.index(KW_2, vi) + len(KW_2)
-			tt_ei = st.index("\"", tt_si)
+            tt_si = st.index(KW_2, vi) + len(KW_2)
+            tt_ei = st.index("\"", tt_si)
 
-			id_si = st.index(KW_1, vi) + len(KW_1)
-			id_ei = pi = st.index(".torrent", id_si)
-		except:
-			break
+            id_si = st.index(KW_1, vi) + len(KW_1)
+            id_ei = pi = st.index(".torrent", id_si)
+        except:
+            break
 
-		rs.append([
-			st[tt_si:tt_ei],
-			st[id_si:id_ei]
-		])
+        rs.append([
+            st[tt_si:tt_ei],
+            st[id_si:id_ei]
+        ])
 
-	for i in range(len(rs) - 1, -1, -1):
-		print("{}. {}".format(i + 1, rs[i][0]))
+    for i in range(len(rs) - 1, -1, -1):
+        print("{}. {}".format(i + 1, rs[i][0]))
 
-	ris = input("i'd like to watch: ").split(" ")
+    ris = input("i'd like to watch: ").split(" ")
 
-	for x in ris:
-		x = int(x)
-		_id = rs[x - 1][1]
+    for x in ris:
+        x = int(x)
+        _id = rs[x - 1][1]
 
-		print("next target is \"{}\", id is {}".format(rs[x - 1][0], _id))
-		dl_by_id(_id)
+        print("next target is \"{}\", id is {}".format(rs[x - 1][0], _id))
+        dl_by_id(_id)
 
 print("done")
 
--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 if [ "$1" == "install" ]; then
-	mkdir -p ~/.local/bin
-	install -m755 nyaa_dl.py ~/.local/bin/nyaa_dl
-	pip install --user -r requirements.txt
+    mkdir -p ~/.local/bin
+    install -m755 nyaa_dl.py ~/.local/bin/nyaa-dl
+    pip install -r requirements.txt
 
-	echo "nyaa_dl was successfully installed"
-	echo "try running nyaa_dl -h"
+    echo "nyaa dl was successfully installed"
+    echo "try running nyaa-dl -h"
 elif [ "$1" == "uninstall" ]; then
-	rm -f ~/.local/bin/nyaa_dl
-	pip uninstall --user -r requirements.txt
+    rm -f ~/.local/bin/{nyaa-dl,nyaa_dl}
 
-	echo "nyaa_dl was successfully uninstalled"
-	echo "sorry to see you go :("
+    echo "nyaa dl was successfully uninstalled"
+    echo "sorry to see you go :("
 elif [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-	echo "usage: ./setup.sh [-h] [--help] [install] [uninstall]"
+    echo "usage: ./setup.sh [-h] [--help] [install] [uninstall]"
 else
-	echo "bad usage"
-	echo "try ./setup.sh -h"
+    echo "bad usage"
+    echo "try ./setup.sh -h"
 fi
+
